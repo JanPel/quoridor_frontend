afe990d0b4b31f27236c94bd401db677890a8edb
Jan Pel <janpel1@hotmail.com>
2024-02-14 16:06:22 +1100

    Try fixing worker again

--- docs/worker.js
+++ docs/worker.js
@@ -1,4 +1,4 @@
-import init from "/./assets/dioxus/Quoridor.js";
+import init from "/quoridor_frontend/assets/dioxus/quoridor_frontend.js";
 
 init("/quoridor_frontend/assets/dioxus/quoridor_frontend_bg.wasm").then(wasm => {
   wasm.start_webworker();
